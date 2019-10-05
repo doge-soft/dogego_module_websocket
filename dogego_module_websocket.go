783e00f664e39178704b15ec6eadaf55f82df877
xiangrui2019 <xiangrui@aiursoft.com>
2019-10-05 04:11:36 -0700

    finish coding

--- dogego_module_websocket.go
+++ dogego_module_websocket.go
@@ -1,7 +1,23 @@
 package dogego_module_websocket
 
-import "net/http"
+import (
+	"github.com/doge-soft/dogego_module_websocket/impls"
+	"github.com/gorilla/websocket"
+	"net/http"
+)
 
-func Upgrade(w http.ResponseWriter, r *http.Request) {
+func Upgrade(w http.ResponseWriter, r *http.Request) (*impls.Connection, error) {
+	upgrader := websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
 
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return impls.NewConnection(conn)
 }
